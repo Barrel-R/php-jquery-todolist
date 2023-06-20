c046e1fc474f2331e42b81d288349c07c79e49f2
Henrique Barrel <henriquebarrel@gmail.com>
2023-06-20 10:55:41 -0300

    first commit, project setup.

---
+++ src/database/Connection.php
@@ -0,0 +1,27 @@
+<?php
+
+class Connection
+{
+    // Database config
+    private $host = 'localhost';
+    private $dbName = 'xneo_crud';
+    private $username = 'root';
+    private $password = '';
+    
+    public function connect() {
+        try {
+            $conn = new PDO("mysql:host={$this->host};dbname={$this->dbName}", $this->username, $this->password);
+        
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $conn->exec('SET NAMES utf8');
+        
+            echo "Connected successfully";
+        
+            return $conn;
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+    
+        }
+    }
+}
---
+++ src/database/create_todo_table.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once './Connection.php';
+
+try {
+    $connection = new Connection();
+
+    $pdo = $connection->connect();
+
+    $sql = "
+    SELECT COUNT(*)
+    FROM information_schema.tables
+    WHERE table_schema = :dbName
+    AND table_name = 'todo_list';
+    ";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':dbName', $dbName, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $tableExists = ($stmt->fetchColumn() > 0);
+
+    if ($tableExists) {
+        echo "To-do list table already exists!";
+    } else {
+        $createTableSql = "
+        CREATE TABLE todo_list (
+            id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            updated_at TIMESTAMP DEFAULT CURRENT TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )
+        ";
+
+        $pdo->exec($createTableSql);
+
+        echo "To-do lists table created successfully!";
+    }
+
+    $pdo = null;
+    $connection = null;
+} catch(PDOException $e) {
+    die("Error creating to-do list table: " . $e->getMessage());
+}
